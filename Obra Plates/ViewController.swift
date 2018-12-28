d4e3daf49fc7d31b971568d7f7bad6637044ea85
Joe Aguilar <jose.aguilar.6694@gmail.com>
2018-12-27 20:59:12 -0800

    Initial Commit

---
+++ Obra Plates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Obra Plates
+//
+//  Created by Jose Aguilar on 12/27/18.
+//  Copyright © 2018 Obra Worldwide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Obra PlatesTests/Obra_PlatesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Obra_PlatesTests.swift
+//  Obra PlatesTests
+//
+//  Created by Jose Aguilar on 12/27/18.
+//  Copyright © 2018 Obra Worldwide. All rights reserved.
+//
+
+import XCTest
+@testable import Obra_Plates
+
+class Obra_PlatesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
